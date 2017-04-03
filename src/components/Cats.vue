9674504026c8223c25d1e4a38be5ddd44e72cbac
Mahesh K S <maheshupama@gmail.com>
2017-04-03 20:19:23 +0530

    Added new file

---
+++ src/components/Cats.vue
@@ -0,0 +1,108 @@
+<template>
+  <div class="hello">
+    <ul id="example-1">
+      <li v-for="male in males">
+        {{ male.name }}
+      </li>
+    </ul>
+    <ul id="example-2">
+      <li v-for="female in females">
+        {{ female.name }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+const APIURL = "http://agl-developer-test.azurewebsites.net/people.json";
+const APIDATA = [];
+
+
+export default {
+  name: 'hello',
+  mounted() {
+    console.log("mounted")
+    this.fetchData()
+  },
+  methods: {
+    fetchData: function() {
+      var self = this;
+      console.log("fetchData")
+      return window.fetch(APIURL)
+        .then(response=> response.json())
+        .then(responsejson => {
+          self.processData(responsejson);
+        })
+        .catch(function(error) {
+            console.log(error);
+        })
+
+    },
+    processData: function(data) {
+      console.log(data)
+      console.log("processdata")
+      var self = this;
+      if (data == undefined) return;
+        var sortedCatsList = {
+            "male" : self.getAllCats(data.filter(subitem => subitem.gender === 'Male')),
+            "female" : self.getAllCats(data.filter(subitem => subitem.gender === 'Female'))
+        }
+        
+
+        this.females = sortedCatsList.female;
+        this.males = sortedCatsList.male;
+        
+    },
+    getAllCats: function(filtereddata){
+      console.log("getAllCats")
+      if (filtereddata == undefined) return [];
+        var sortList = [];
+        filtereddata.map(item => {
+          var catArr =  (item.pets || []).filter(pet => pet.type === 'Cat');
+          catArr.map(cat => {
+               sortList.push(cat);
+           });
+        })
+      return sortList.sort(function(a,b) {return (a.name > b.name) ? 1 : ((b.name > a.name) ? -1 : 0);} );
+    }
+  },
+  watch: {
+    watchMe: function() {
+      console.log("watch");
+      this.fetchData();
+    }
+  },
+  created() {
+    console.log("data")
+  },
+  data() {
+    console.log("data")
+    return {
+      watchMe: "",
+      females: [],
+      males: []
+    };
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
