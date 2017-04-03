d38ab5bc249fd569855b7a921f6131aa36d810eb
Mahesh K S <maheshupama@gmail.com>
2017-04-03 20:20:40 +0530

    add app.vue file

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <h1>Cats grouped under owner's gender</h1>
+    <cats data="test"></cats>
+    
+  </div>
+</template>
+
+<script>
+import Cats from './components/Cats';
+
+
+export default {
+  name: 'app',
+  components: {
+    Cats,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
