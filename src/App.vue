495fc45a7f6ee9cfecb3bb7d3c049aecca93c0a2
Krishang <Krishang>
2017-04-03 08:21:13 +0530

    Pushing all inital set up files to git

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <h1>Cats grouped under owner's gender</h1>
+    <hello data="test"></hello>
+    
+  </div>
+</template>
+
+<script>
+import Hello from './components/Hello';
+
+
+export default {
+  name: 'app',
+  components: {
+    Hello,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
